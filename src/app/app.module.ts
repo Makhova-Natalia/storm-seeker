a96b2a2b237d9379b8f1d6e563540b3a21122347
Makhova Natalia <nataliahollt@gmail.com>
2024-04-11 11:04:18 +0300

    02-create-header. Create HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from "./header/header.component";
+import { MaterialModule } from "./material-module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
