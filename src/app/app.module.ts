e79d9eb800c53e2ae8864e1049870e014bd41dc9
Makhova Natalia <nataliahollt@gmail.com>
2024-04-11 12:13:03 +0300

    02-create-header. Create Favorites, Home components. Add links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./header/header.component";
 import { MaterialModule } from "./material-module";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { MaterialModule } from "./material-module";
   imports: [
     BrowserModule,
     MaterialModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent],
