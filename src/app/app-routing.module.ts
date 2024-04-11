ee91c9c941dd9c934cd49be1610a77727808a451
Makhova Natalia <nataliahollt@gmail.com>
2024-04-11 18:39:38 +0300

    03-create-template-for-home-component. Use state management for folders structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { FavoritesComponent } from "./favorites/favorites.component";
+import { HomeComponent } from "./features/home/home.component";
+import { FavoritesComponent } from "./features/favorites/favorites.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
 import { MaterialModule } from "./material-module";
 
 @NgModule({
