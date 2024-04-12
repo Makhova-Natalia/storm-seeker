691062e90675571553f78dbb84df1d4fbe87d472
Makhova Natalia <nataliahollt@gmail.com>
2024-04-12 21:31:01 +0300

    03-create-template-for-home-component. Add components to template

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { SearchLocationComponent } from "./components/search-location/search-location.component";
 import { MaterialModule } from "../../material-module";
+import { AddFavoriteComponent } from "./components/add-favorite/add-favorite.component";
+import { CurrentForecastComponent } from "./components/current-forecast/current-forecast.component";
+import { FutureForecastsComponent } from "./components/future-forecasts/future-forecasts.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ SearchLocationComponent, MaterialModule ],
+  imports: [
+    SearchLocationComponent,
+    MaterialModule,
+    AddFavoriteComponent,
+    CurrentForecastComponent,
+    FutureForecastsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
