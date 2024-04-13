7f4328141e7a290222401719b4c6edb410c16baa
Makhova Natalia <nataliahollt@gmail.com>
2024-04-13 16:37:44 +0300

    04-add-logic-to-search-location-component. Improve logic

--- src/app/features/home/components/search-location/search-location.component.ts
+++ src/app/features/home/components/search-location/search-location.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MaterialModule } from "../../../../material-module";
 import { FormsModule } from "@angular/forms";
 import { WeatherService } from "../../../../core/services/weather.service";
 import { SearchResult, WeatherConditions } from "../../../../core/models/weather.model";
-import { Observable } from "rxjs";
+import { Observable, Subject, takeUntil } from "rxjs";
 
 @Component({
   selector: 'app-search-location',
@@ -12,36 +12,51 @@ import { Observable } from "rxjs";
   templateUrl: './search-location.component.html',
   styleUrl: './search-location.component.css'
 })
-export class SearchLocationComponent implements OnInit {
+export class SearchLocationComponent implements OnInit, OnDestroy {
   readonly defaultCity: string = 'Kiev';
+  private destroyed$$: Subject<void> = new Subject<void>();
 
-  searchQuery: string = this.defaultCity;
-  searchResult$: Observable<SearchResult>;
-  currentConditions$: Observable<WeatherConditions>;
+  cityName: string = this.defaultCity;
+  // searchResult$: Observable<SearchResult>;
+  // currentConditions$: Observable<WeatherConditions>;
 
   constructor(private weatherService: WeatherService) {
   }
 
   ngOnInit() {
-    this.searchResult$ = this.weatherService.getSearchResult();
-    this.currentConditions$ = this.weatherService.getCurrentConditions();
+    // this.searchResult$ = this.weatherService.getSearchResult();
+    // this.currentConditions$ = this.weatherService.getCurrentConditions();
 
-    this.weatherService.searchLocation(this.defaultCity).subscribe(val => {
+    this.fetchWeatherData(this.defaultCity);
+  }
+
+  private fetchWeatherData(cityName: string): void {
+    this.weatherService.searchLocation(cityName)
+      .pipe(
+        takeUntil(this.destroyed$$)
+      )
+      .subscribe(val => {
       this.weatherService.setSearchResult(val[0]);
       this.getWeatherConditions(val[0].Key);
     })
   }
 
   searchLocation(): void {
-    this.weatherService.searchLocation(this.searchQuery).subscribe(val => {
-      this.weatherService.setSearchResult(val[0]);
-      this.getWeatherConditions(val[0].Key);
-    });
+    this.fetchWeatherData(this.cityName);
   }
 
   getWeatherConditions(locationKey: string): void {
-    this.weatherService.getCurrentWeatherConditions(locationKey).subscribe(val => {
+    this.weatherService.getCurrentWeatherConditions(locationKey)
+      .pipe(
+        takeUntil(this.destroyed$$)
+      )
+      .subscribe(val => {
       this.weatherService.setCurrentConditions(val[0]);
     });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$$.next();
+    this.destroyed$$.complete();
   }
 }
