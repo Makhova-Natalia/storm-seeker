f74e5d07d76b391160521f293466a39d13bf17bf
Makhova Natalia <nataliahollt@gmail.com>
2024-04-13 17:30:12 +0300

    04-add-logic-to-search-location-component. Add dataCached

--- src/app/core/services/weather.service.ts
+++ src/app/core/services/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, of, tap } from "rxjs";
 import { environment } from "../../../environments/environment";
 import { SearchResult, WeatherConditions } from "../models/weather.model";
 
@@ -11,6 +11,8 @@ import { SearchResult, WeatherConditions } from "../models/weather.model";
 export class WeatherService {
   private searchResult$$: BehaviorSubject<SearchResult> = new BehaviorSubject<SearchResult>({} as SearchResult);
   private currentConditions$$: BehaviorSubject<WeatherConditions> = new BehaviorSubject<WeatherConditions>({} as WeatherConditions);
+  private cachedSearchResult: SearchResult[];
+  private cachedCurrentConditions: WeatherConditions[];
 
   readonly API_HOST = environment.API_HOST;
   readonly API_KEY = environment.API_KEY;
@@ -39,15 +41,33 @@ export class WeatherService {
     return this.currentConditions$$.asObservable();
   }
 
-  searchLocation(query: string): Observable<any> {
-    return this.http.get(`${this.API_HOST}/${this.URL_BODIES.autocomplete}`, {
-      params: { apikey: this.API_KEY, q: query }
-    });
+  searchLocation(query: string): Observable<SearchResult[]> {
+    if(environment.production || !this.cachedSearchResult) {
+      return this.http.get<SearchResult[]>(`${this.API_HOST}/${this.URL_BODIES.autocomplete}`, {
+        params: {apikey: this.API_KEY, q: query}
+      })
+        .pipe(
+          tap((response: SearchResult[]) => {
+            this.cachedSearchResult = response;
+          })
+        );
+    } else {
+      return of(this.cachedSearchResult);
+    }
   }
 
-  getCurrentWeatherConditions(locationKey: string): Observable<any> {
-    return this.http.get(`${this.API_HOST}/${this.URL_BODIES.currentWeather}/${locationKey}`, {
-      params: { apikey: this.API_KEY }
-    });
+  getCurrentWeatherConditions(locationKey: string): Observable<WeatherConditions[]> {
+    if (environment.production || !this.cachedCurrentConditions) {
+      return this.http.get<WeatherConditions[]>(`${this.API_HOST}/${this.URL_BODIES.currentWeather}/${locationKey}`, {
+        params: {apikey: this.API_KEY}
+      })
+        .pipe(
+          tap((response: WeatherConditions[]) => {
+            this.cachedCurrentConditions = response;
+          })
+        );
+    } else {
+      return of(this.cachedCurrentConditions);
+    }
   }
 }
